98279bd0d50561081f941ca68514c8be5ba26a80
Vesa Marttila <vesa.marttila@helsinki.fi>
2009-11-06 00:35:09 +0200

    Listing of all courses DONE, own pages with description DONE

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,11 @@
 class CoursesController < ApplicationController
-  def index
-      @courses = Course.find(:all)
-  end
+    def index
+        @courses = Course.find(:all)
+        @time = Time.now
+    end
+
+    def show
+        @course = Course.find(params[:id])
+        @time = Time.now
+    end
 end
